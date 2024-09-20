d395fb81485439e0e134c4ef003c56a65a289d4b
tejaskl <43215767+tejaskl@users.noreply.github.com>
2024-09-19 19:57:22 -0700

    user controller with login, logout, and create account

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,113 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export const register = async (req, res) => {
+    try {
+        const { fullname, email, phoneNumber, password, role } = req.body;
+        if (!fullname || !email || !phoneNumber || !password || !role) {
+            return res.status(400).json({
+                message: "Data is incomplete",
+                success: false
+            });
+        };
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                message: "User already exists.",
+                success: false
+            });
+        };
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await User.create({
+            fullname,
+            email,
+            phoneNumber,
+            password: hashedPassword,
+            role
+        });
+        return res.status(200).json({
+            message: "Account created successfully",
+            success: true
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+export const login = async (req, res) => {
+    try {
+        const { email, password, role } = req.body;
+        if (!email || !password || !role) {
+            return res.status(400).json({
+                message: "Please fill in all the details",
+                success: false
+            });
+        };
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "User doesn't exist!",
+                success: false
+            });
+        };
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({
+                message: "Incorrect email.",
+                success: false
+            });
+        };
+
+        //check role is correct or not
+
+        if (role != user.role) {
+            return res.status(400).json({
+                message: "Role doesn't match.",
+                success: false
+            });
+        };
+
+        const tokenData = {
+            userId: user._id
+        }
+        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
+
+        user = {
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+
+        }
+
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.fullname}`,
+            user,
+            success: true
+        });
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logges Out Successfully",
+            success: true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
